35a6275641aa8e947e516642cec1b26c6d173304
gearcoded <cipherplain@yandex.com>
2023-06-02 13:10:04 +0500

    Updated Gantt to 8.0.2

--- force-app/main/default/lwc/gantt/gantt.js
+++ force-app/main/default/lwc/gantt/gantt.js
@@ -6,7 +6,7 @@ import { loadStyle, loadScript } from "lightning/platformResourceLoader";
 import { createRecord, updateRecord, deleteRecord } from "lightning/uiRecordApi";
  
 // Static resources
-import GanttFiles from "@salesforce/resourceUrl/dhtmlxgantt7111";
+import GanttFiles from "@salesforce/resourceUrl/dhtmlxgantt802";
  
 // Controllers
 import getTasks from "@salesforce/apex/GanttData.getTasks";
